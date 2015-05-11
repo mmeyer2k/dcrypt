06c72b54ff5813c5e064f54de890e660bbc21c96
Michael M <m.meyer2k@gmail.com>
2015-05-10 21:03:47 -0500

    Update OtpTest.php

--- tests/OtpTest.php
+++ tests/OtpTest.php
@@ -5,6 +5,17 @@ use Dcrypt\Otp;
 class OtpTest extends PHPUnit_Framework_TestCase
 {
 
+    public function testCompatibility()
+    {
+        
+        $h = hex2bin('cfceb8891c58071deedbd1');
+        $k = 'key123'
+        $p = 'hello world';
+        
+        $this->assertEquals($p, Otp::crypt($h, $k))
+        
+    }
+
     public function testCrypt()
     {
         foreach(array(1, 1000, 100000) as $mult) {
