61e9076170656febe5e469b013fb7cb61dc7bddf
Michael M <m.meyer2k@gmail.com>
2015-05-20 14:43:11 -0500

    Update Strcmp.php

--- src/Strcmp.php
+++ src/Strcmp.php
@@ -25,6 +25,7 @@ namespace Dcrypt;
  * @author   Michael Meyer (mmeyer2k) <m.meyer2k@gmail.com>
  * @license  http://opensource.org/licenses/MIT The MIT License (MIT)
  * @link     https://github.com/mmeyer2k/dcrypt
+ * @link     https://github.com/symfony/Security/blob/master/Core/Util/StringUtils.php
  */
 class Strcmp
 {
