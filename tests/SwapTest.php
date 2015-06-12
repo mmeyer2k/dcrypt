51b7c73e3c39cd32757ee74b7a610ed84e7f5f77
Michael M <m.meyer2k@gmail.com>
2015-06-12 13:53:44 -0500

    Update SwapTest.php

--- tests/SwapTest.php
+++ tests/SwapTest.php
@@ -8,7 +8,7 @@ class SwapTest extends PHPUnit_Framework_TestCase
     public function testSwap()
     {
         $orig = 'AAAAAAAAAA';
-        for ($i = 0; $i < 500; $i = $i + 5) {
+        for ($i = 0; $i < 10; $i = $i + 5) {
             $this->assertFalse($orig === swaprandbyte($orig));
             $this->assertEquals(levenshtein($orig, swaprandbyte($orig)), 1);
         }
