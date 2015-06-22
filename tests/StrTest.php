5931dfdc53526d0bb3039e21879b4d19e58f2a46
Michael M <m.meyer2k@gmail.com>
2015-06-21 21:25:58 -0500

    Update StrTest.php

--- tests/StrTest.php
+++ tests/StrTest.php
@@ -5,8 +5,8 @@ class StrcmpTest extends PHPUnit_Framework_TestCase
 {
     public function testSpecial()
     {
-        // This function tests the conditional type casting of the
-        // user input in str::equal
+        // This function tests the conditionals from symfony
         $this->assertTrue(Str::equal('2222', 2222));
+        $this->assertTrue(Str::equal(2222, '2222'));
     }
 }
