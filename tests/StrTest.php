9017664c7767caf0248bd6789e337ecc74b52287
Michael M <m.meyer2k@gmail.com>
2015-06-22 15:53:36 -0500

    Update StrTest.php

--- tests/StrTest.php
+++ tests/StrTest.php
@@ -1,15 +1,22 @@
 <?php
+
 use Dcrypt\Str;
 
 class StrcmpTest extends PHPUnit_Framework_TestCase
 {
-    public function testSpecial()
+
+    public function testEquals()
     {
         // This function tests the conditionals from symfony
         $this->assertTrue(Str::equal('2222', 2222));
         $this->assertTrue(Str::equal(2222, '2222'));
-        
+
+        // Test falseness
+        $this->assertFalse(Str::equal('2222', '3333', true));
+
         // Test without hash_equals
         $this->assertTrue(Str::equal('2222', '2222', false));
+        $this->assertFalse(Str::equal('2222', '3333', false));
     }
+
 }
