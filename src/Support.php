1610f7c775d77c51956e2dfb0dce6a029ca9f4db
Michael M <m.meyer2k@gmail.com>
2015-06-30 23:09:24 -0500

    Update Support.php

--- src/Support.php
+++ src/Support.php
@@ -69,11 +69,7 @@ class Support extends Str
         while ($i < $n) {
             $a = substr($hexstr, $i, 2);
             $c = pack('H*', $a);
-            if ($i == 0) {
-                $sbin = $c;
-            } else {
-                $sbin.= $c;
-            }
+            $sbin.= $c;
             $i+=2;
         }
 
