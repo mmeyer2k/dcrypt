b0be983719b8bd1939ca684a9b2a2785452402c2
Michael M <m.meyer2k@gmail.com>
2015-07-21 17:58:49 -0500

    Update Cryptobase.php

--- src/Cryptobase.php
+++ src/Cryptobase.php
@@ -14,6 +14,15 @@
 
 namespace Dcrypt;
 
+/**
+ * Provides functionality common to Dcrypt's block ciphers.
+ * 
+ * @category Dcrypt
+ * @package  Dcrypt
+ * @author   Michael Meyer (mmeyer2k) <m.meyer2k@gmail.com>
+ * @license  http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link     https://github.com/mmeyer2k/dcrypt
+ */
 class Cryptobase extends Str
 {
 
