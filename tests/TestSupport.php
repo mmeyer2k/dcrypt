9144223e53384367b3e08504c0d96f560edcfbb7
Michael M <m.meyer2k@gmail.com>
2017-01-31 23:20:03 -0600

    Update TestSupport.php

--- tests/TestSupport.php
+++ tests/TestSupport.php
@@ -4,7 +4,7 @@ class TestSupport extends PHPUnit_Framework_TestCase
 {
     public static function mcryptDeprecated() 
     {
-        return version_compare(PHP_VERSION, '7.1.0') >= 0;
+        return function_exists('mcrypt_decrypt') && function_exists('mcrypt_get_iv_size');
     }
     
     public static function mcryptCiphers()
