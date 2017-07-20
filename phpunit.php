d0ffcf39ab5baad0e3913e2072845a40264941e7
mike <m.meyer2k@gmail.com>
2017-07-20 15:22:36 -0500

    no message

--- phpunit.php
+++ phpunit.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/vendor/autoload.php';
 
 // backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
+if (!class_exists('\PHPUnit\Framework\TestCase')) {
     class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
 }
 
