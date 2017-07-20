ed29c8e13f284470c60b269ce0f363c30490592f
mike <m.meyer2k@gmail.com>
2017-07-20 15:23:53 -0500

    no message

--- phpunit.php
+++ phpunit.php
@@ -4,7 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 // backward compatibility
 if (!class_exists('\PHPUnit\Framework\TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+    class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
 
 date_default_timezone_set('UTC');
