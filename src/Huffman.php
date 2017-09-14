7e95fa55300ee16ba0e5b44c3e3d8e9f31ceb0d3
mmeyer2k <m.meyer2k@gmail.com>
2017-09-14 02:44:52 -0500

    Update Huffman.php

--- src/Huffman.php
+++ src/Huffman.php
@@ -1,8 +1,30 @@
 <?php
 
+/**
+ * Huffman.php
+ * 
+ * PHP version 5
+ * 
+ * @category Dcrypt
+ * @package  Dcrypt
+ * @author   Michael Meyer (mmeyer2k) <m.meyer2k@gmail.com>
+ * @license  http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link     https://github.com/mmeyer2k/dcrypt
+ */
+
 namespace Dcrypt;
 
-class Huffman
+/**
+ * Huffman compression functions.
+ *
+ * @category Dcrypt
+ * @package  Dcrypt
+ * @author   Michael Meyer (mmeyer2k) <m.meyer2k@gmail.com>
+ * @license  http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link     https://github.com/mmeyer2k/dcrypt
+ * @link     https://apigen.ci/github/mmeyer2k/dcrypt/namespace-Dcrypt.html
+ */
+final class Huffman
 {
 
     /**
