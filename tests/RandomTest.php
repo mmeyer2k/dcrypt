bfcb5e52cd2728ca2714a1013feca60dc15e1044
mmeyer2k <m.meyer2k@gmail.com>
2017-11-26 12:06:50 -0600

    PHP 7.1+ fix

--- tests/RandomTest.php
+++ tests/RandomTest.php
@@ -15,7 +15,7 @@ class RandomTest extends \PHPUnit\Framework\TestCase
     {
         $array = array('a', 'b', 'c', 'd');
 
-        $array = \Dcrypt\Random::shuffle($array, 'seed string can be any length because it is hashed before use');
+        $array = \Dcrypt\Random::shuffle($array, 'seed string can be any length because it is hashed before use', false);
         
         $this->assertEquals('b', array_shift($array));
         $this->assertEquals('a', array_shift($array));
