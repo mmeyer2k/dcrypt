621fe3a044f37962a1120ab8f128d30f153fec39
mmeyer2k <m.meyer2k@gmail.com>
2018-01-20 23:35:54 -0600

    Update Cryptobase.php

--- src/Cryptobase.php
+++ src/Cryptobase.php
@@ -38,7 +38,7 @@ class Cryptobase
      *
      * @return string
      */
-    protected static function checksum($cyphertext, $iv, $key, $cipher = 'rijndael-128', $mode = 'cbc', $algo = 'sha256')
+    protected static function checksum(string $cyphertext, string $iv, string $key, $cipher = 'rijndael-128', $mode = 'cbc', $algo = 'sha256'): string
     {
         // Prevent potentially large string concat by hmac-ing the cyphertext
         // by itself...
