931a08d1bd865539fd9bfd2b4f0726cf6b976445
mmeyer2k <m.meyer2k@gmail.com>
2018-02-28 21:48:39 -0600

    no message

--- tests/TestSupport.php
+++ tests/TestSupport.php
@@ -2,48 +2,6 @@
 
 class TestSupport extends \PHPUnit\Framework\TestCase
 {
-    public static function mcryptDeprecated()
-    {
-        if (version_compare(PHP_VERSION, '7.1.0') >= 0) {
-            return true;
-        }
-        
-        return !function_exists('mcrypt_decrypt') || !function_exists('mcrypt_get_iv_size');
-    }
-
-    public static function mcryptCiphers()
-    {
-        return array(
-            MCRYPT_3DES,
-            MCRYPT_BLOWFISH,
-            MCRYPT_BLOWFISH_COMPAT,
-            MCRYPT_DES,
-            MCRYPT_LOKI97,
-            MCRYPT_CAST_128,
-            MCRYPT_CAST_256,
-            MCRYPT_RC2,
-            MCRYPT_RIJNDAEL_128,
-            MCRYPT_RIJNDAEL_192,
-            MCRYPT_RIJNDAEL_256,
-            MCRYPT_SAFERPLUS,
-            MCRYPT_SERPENT,
-            MCRYPT_TRIPLEDES,
-            MCRYPT_TWOFISH,
-            MCRYPT_XTEA,
-        );
-    }
-
-    public static function mcryptModes()
-    {
-        return array(
-            MCRYPT_MODE_CBC,
-            MCRYPT_MODE_CFB,
-            MCRYPT_MODE_ECB,
-            MCRYPT_MODE_OFB,
-            MCRYPT_MODE_NOFB,
-        );
-    }
-
     /**
      * Change a random byte, randomly. This function is used in unit testing
      * only and never in the namespaced areas of code.
