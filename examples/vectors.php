d62a06eb040536030c70059f2ee735085d6a4c47
mmeyer2k <m.meyer2k@gmail.com>
2019-06-25 20:00:33 -0500

    no message

--- examples/vectors.php
+++ examples/vectors.php
@@ -1,11 +1,20 @@
-<?php
+<?php declare(strict_types=1);
+
+use \Dcrypt\OpensslStatic;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$abcd = OpensslStatic::encrypt('hello', 'world', 'aes-256-cbc', 'sha256', 10);
+$efgh = OpensslStatic::decrypt($abcd, 'world', 'aes-256-cbc', 'sha256', 10);
 
 $out = [];
 
 foreach (\Dcrypt\OpensslSupported::ciphers() as $cipher) {
-    $out[strtolower($cipher)] = base64_encode(\Dcrypt\OpensslStatic::encrypt('hello', 'world', $cipher, 'sha256', 1));
+    $cipher = strtolower($cipher);
+    if (isset($out[$cipher])) {
+        continue;
+    }
+    $out[$cipher] = base64_encode(OpensslStatic::encrypt('hello', 'world', $cipher, 'sha256', 10));
 }
 
 file_put_contents(__DIR__ . '/../tests/vectors/openssl-static-ciphers.json', \json_encode($out, JSON_PRETTY_PRINT));
@@ -13,7 +22,11 @@ file_put_contents(__DIR__ . '/../tests/vectors/openssl-static-ciphers.json', \js
 $out = [];
 
 foreach (\Dcrypt\OpensslSupported::algos() as $algo) {
-    $out[strtolower($algo)] = base64_encode(\Dcrypt\OpensslStatic::encrypt('hello', 'world', 'aes-256-gcm', $algo, 1));
+    $cipher = strtolower($algo);
+    if (isset($out[$algo])) {
+        continue;
+    }
+    $out[$algo] = base64_encode(OpensslStatic::encrypt('hello', 'world', 'aes-256-gcm', $algo, 10));
 }
 
 file_put_contents(__DIR__ . '/../tests/vectors/openssl-static-algos.json', \json_encode($out, JSON_PRETTY_PRINT));
