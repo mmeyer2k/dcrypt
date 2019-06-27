ed86a2fe5c577bd47cda0d8ab5d8ede8e4001b18
mmeyer2k <m.meyer2k@gmail.com>
2019-06-26 23:50:32 -0500

    no message

--- examples/vectors.php
+++ examples/vectors.php
@@ -3,10 +3,7 @@
 use \Dcrypt\OpensslStatic;
 
 require __DIR__ . '/../vendor/autoload.php';
-
-$abcd = OpensslStatic::encrypt('hello', 'world', 'aes-256-cbc', 'sha256', 10);
-$efgh = OpensslStatic::decrypt($abcd, 'world', 'aes-256-cbc', 'sha256', 10);
-
+/*
 $out = [];
 
 foreach (\Dcrypt\OpensslSupported::ciphers() as $cipher) {
@@ -18,7 +15,7 @@ foreach (\Dcrypt\OpensslSupported::ciphers() as $cipher) {
 }
 
 file_put_contents(__DIR__ . '/../tests/vectors/openssl-static-ciphers.json', \json_encode($out, JSON_PRETTY_PRINT));
-
+*/
 $out = [];
 
 foreach (\Dcrypt\OpensslSupported::algos() as $algo) {
