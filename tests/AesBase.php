b52efa0c7490bc73d945377e0c97659c825534d3
mike <m.meyer2k@gmail.com>
2020-10-20 19:58:40 -0500

    📞 change call to obsolete phpunit assertion 📐 fix single broken test vector for ccm mode due to php updates

--- tests/AesBase.php
+++ tests/AesBase.php
@@ -56,7 +56,7 @@ class AesBase extends \PHPUnit\Framework\TestCase
         $testname1 = strtolower(str_replace('-', '', static::$class::CIPHER));
         $testname2 = strtolower(static::$class);
 
-        $this->assertStringContainsString($testname1, $testname2);
+        $this->assertContains($testname1, $testname2);
     }
 
     public function testKnownVector()
