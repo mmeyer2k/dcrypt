1cd3804cc5254d333b5ab03a3ee3801513211a2a
mmeyer2k <1887431+mmeyer2k@users.noreply.github.com>
2023-08-24 14:07:33 -0500

    🛠️fix php81 test

--- tests/OpensslStaticTest.php
+++ tests/OpensslStaticTest.php
@@ -35,7 +35,21 @@ class OpensslStaticTest extends TestCase
 
         $json = json_decode($json);
 
+        // Skip the ciphers that are treated differently by PHP 8.1
+        $skip81 = [
+            'aes-256-ccm',
+            'aes-256-cbc-hmac-sha1',
+            'aes-256-cbc-hmac-sha256',
+            'aes-128-ccm',
+            'aes-128-cbc-hmac-sha1',
+            'aes-128-cbc-hmac-sha256',
+        ];
+
         foreach ($json->ciphers as $cipher => $ciphertext) {
+            if (version_compare(PHP_VERSION, '8.1.0') >= 0 && in_array($cipher, $skip81)) {
+                continue;
+            }
+
             try {
                 $plaintext = OpensslStatic::decrypt(base64_decode($ciphertext), $json->key, $cipher, 'sha3-256');
             } catch (\Exception $e) {
